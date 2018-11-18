6af1c36373246231e3a910e832280325fcceda04
sritala1 <susanna.ritala@helsinki.fi>
2018-11-18 18:41:26 +0200

    muutoksia

--- application/views.py
+++ application/views.py
@@ -2,11 +2,10 @@ from flask import render_template, request, redirect, url_for
 
 from application import app, db
 from application.tasks.models import Task
-from application.tasks.forms import TaskForm
 
-@app.route("/tasks/new/")
-def tasks_form():
-    return render_template("tasks/new.html", form = TaskForm())
+#@app.route("/tasks/create/")
+#def tasks_form():
+#   return render_template("tasks/new.html", form = TaskForm())
 
 @app.route("/")
 def index():
