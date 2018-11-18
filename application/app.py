73489f752af6b1dc68a8b8c50c019765e3dea6ac
sritala1 <susanna.ritala@helsinki.fi>
2018-11-18 16:54:49 +0200

    viikko3

---
+++ application/app.py
@@ -0,0 +1,47 @@
+from flask import Flask
+app = Flask(__name__)
+
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"    
+    app.config["SQLALCHEMY_ECHO"] = True
+
+  
+db = SQLAlchemy(app)
+
+from application import views
+
+from application.tasks import models
+from application.tasks import views
+
+from application.auth import models
+from application.auth import views
+
+
+# login
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+try: 
+    db.create_all()
+except:
+    pass
+
---
+++ application/auth/views.py
@@ -0,0 +1,18 @@
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user
+
+from application import app, db
+from application.auth.models import User
+from application.auth.forms import LoginForm
+
+@app.route("/auth/login", methods = ["GET", "POST"])
+def auth_login():
+    # aiemmin toteutettu kirjautumistoiminnallisuus
+  
+    return redirect(url_for("index"))    
+
+
+@app.route("/auth/logout")
+def auth_logout():
+    logout_user()
+    return redirect(url_for("index")) 
