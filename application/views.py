7e31b70f0901b6d85493a071d0ec5a88f71147e0
sritala1 <susanna.ritala@helsinki.fi>
2018-12-02 19:26:38 +0200

    Heroku fix attempt 3

--- application/views.py
+++ application/views.py
@@ -2,7 +2,6 @@ from flask import render_template
 from application import app
 from application.auth.models import User
 
-
 @app.route('/')
 def index():
     return render_template("index.html", needs_projects=User.find_users_with_no_projects())
