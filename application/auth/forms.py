6ac9fbe4ce53d99a60be79d6f3449abbc5199a5c
sritala <susanna.ritala@hotmail.com>
2019-07-26 15:11:09 +0300

    initial commit

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -12,8 +12,8 @@ class LoginForm(FlaskForm):
 class RegisterForm(FlaskForm):
     name = StringField("Name", [validators.Length(min=5)], render_kw={"placeholder": "Name"})
     username = StringField("Username",[validators.Length(min=2)],render_kw={"placeholder": "Username"})
-    password = PasswordField("Password",[validators.Length(min=6)], render_kw={"placeholder": "password"})
-    passwordAgain = PasswordField("Type your password again",[validators.Length(min=6)], render_kw={"placeholder": "Type your password again"})
+    password = PasswordField("Password",[validators.Length(min=6)], render_kw={"placeholder": "Password"})
+    passwordAgain = PasswordField("Password again",[validators.Length(min=6)], render_kw={"placeholder": "Type your password again"})
 
     class Meta:
         csrf = False
